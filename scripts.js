e12d2469a146cc4a2e81c121513a9b960a2e1c3c
vicXle <vkle1@asu.edu>
2023-09-28 14:07:06 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ document.getElementById("firstPara").innerHTML += "<span class=\"tan\"> This is 
 //tag name section
 // get all images and change width to 250px
 let images = document.getElementsByTagName("img");
-console.log(images);
+// console.log(images);
 
 for(let image of images){
     image.width = "250";
@@ -27,3 +27,23 @@ for(let span of medBlueSpans){
 }
 
 // get elements by css selector
+let colors = ["#283040", "#3C5E73", "#7C9EA6", "#D9BCA3", "#F2DCC9"];
+
+let svgs = document.querySelectorAll("#svgs svg");
+// console.log(svgs);
+
+// iterate through the svg collection and change color of each
+for(let i = 0; i < svgs.length; i++){
+    svgs[i].style.stroke = colors[i];
+}
+
+// individual element with css selector
+document.querySelector(".bold").style.color = "#7C9EA6";
+
+// changing the dom node content
+document.querySelector(".content_list li:first-of-type").textContent = "This is new list item text";
+
+document.querySelectorAll(".content_list li")[2].innerHTML += "<strong> Victoria</strong>";
+
+// removing attributes
+document.querySelector("#remove a").removeAttribute("hidden");
